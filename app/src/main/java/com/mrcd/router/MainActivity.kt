d8dcb5efff8927ce98def7b30b7731044fee4909
代圣达 <dsd13502@gmail.com>
2022-01-07 20:33:02 +0800

    对接测试失败

--- app/src/main/java/com/mrcd/router/MainActivity.kt
+++ app/src/main/java/com/mrcd/router/MainActivity.kt
@@ -2,7 +2,9 @@ package com.mrcd.router
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.mrcd.router.annotation.Router
 
+@Router(path = "测试 url")
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
